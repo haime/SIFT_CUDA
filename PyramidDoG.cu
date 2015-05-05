e42d521b1a44fc153ba8ec42ff8af9424b6b90e0
haime <harime.fi@gmail.com>
2015-05-05 14:25:27 -0500

    correccion memoria en proceso

--- PyramidDoG.cu
+++ PyramidDoG.cu
@@ -3,70 +3,60 @@
 
 
 
-__global__ void ConvolutionDoG(ArrayImage* images,ArrayImage* mask, ArrayImage* PyDoG, int idxImages, int maskSize){
+__global__ void Blur(float* image,float* mask, ArrayImage* PyDoG, int maskR,int maskC, int imgR,int imgC, float* imgOut, int idxPyDoG){
 	int tid= threadIdx.x;
 	int bid= blockIdx.x;
 	int bDim=blockDim.x;
 	int gDim=gridDim.x;
-	int idxPyDoG = idxImages*maskSize;
 	
-	
-	for (int idxMask = 0; idxMask < maskSize; ++idxMask)
-	{
-		int iImg=0;
-		float aux=0;
-		int pxlThrd = ceil((double)(images[idxImages].cols*images[idxImages].rows)/(gDim*bDim)); ////////numero de veces que caben
-																								 ////////los hilos en la imagen.
 		
+	int iImg=0;
+	float aux=0;
+	int pxlThrd = ceil((double)(imgC*imgR)/(gDim*bDim)); ////////numero de veces que caben
+														 ////////los hilos en la imagen.
 	
-		for (int i = 0; i <pxlThrd; ++i)///////////////////////////// Strike 
-		{
-			//////////////////////////////////////
-			//////////////////////////////////////Calculo de indices
-			iImg=(tid+(bDim*bid)) + (i*(gDim*bDim)); //// pixel en el que trabajara el hilo
-			//////////////////////////////////////
-			//////////////////////////////////////
-			if(iImg < images[idxImages].cols*images[idxImages].rows){
-				
-				int condition=mask[idxMask].cols/2+images[idxImages].cols*(floor((double)mask[idxMask].cols/2));
-				
-				
-
-				if (iImg-condition < 0  ||												///condicion arriba
-					iImg+condition > images[idxImages].cols*images[idxImages].rows ||	///condicion abajo
-					iImg%images[idxImages].cols < mask[idxMask].cols/2 ||				///condicion izquierda
-					iImg%images[idxImages].cols >=mask[idxMask].cols/2 )					///condicion derecha
-				{
-					aux=0;
-					
-				}else{		
-					
-					int itMask = 0;
-					int itImg=iImg-condition;
-
-					
-					for (int j = 0; j < mask[idxMask].rows; ++j)
-					{		
-						for (int h = 0; h < mask[idxMask].cols; ++h)
-						{
-							aux+=images[idxImages].image[itImg]*mask[idxMask].image[itMask];
-							++itMask;
-							++itImg;
-						}
-						itImg+=images[idxImages].cols-mask[idxMask].cols;
-					}
-					
-					aux=0;
-				}
-				
-				PyDoG[idxPyDoG].image[iImg]=aux;
+	for (int i = 0; i <pxlThrd; ++i)///////////////////////////// Strike 
+	{
+		//////////////////////////////////////
+		//////////////////////////////////////Calculo de indices
+		iImg=(tid+(bDim*bid)) + (i*(gDim*bDim)); //// pixel en el que trabajara el hilo
+		//////////////////////////////////////
+		//////////////////////////////////////
+		if(iImg < imgC*imgR){
+			int condition=maskC/2+imgC*(floor((double)maskC/2));
+			if (iImg-condition < 0  ||												///condicion arriba
+				iImg+condition > imgC*imgR ||	///condicion abajo
+				iImg%imgC < maskC/2 ||				///condicion izquierda
+				iImg%imgC >=maskC/2 )					///condicion derecha
+			{
 				aux=0;
-
+				
+			}else{		
+				int itMask = 0;
+				int itImg=iImg-condition;
+				for (int j = 0; j < maskR; ++j)
+				{		
+					for (int h = 0; h < maskC; ++h)
+					{
+						aux+=image[itImg]*mask[idxMask].image[itMask];
+						++itMask;
+						++itImg;
+					}
+					itImg+=imgC-maskC;
+				}
 			}
-		}
-		++idxPyDoG;
+			imgOut[iImg]=aux;
 
+			PyDoG[idxPyDoG].image=imgOut;
+			PyDoG[idxPyDoG].cols=imgC;
+			PyDoG[idxPyDoG].rows=imgR;
+			aux=0;
+
+		}
 	}
+	
+
+
 }
 
 
@@ -89,7 +79,6 @@ int ResizeImage(Mat image,vector<Mat>& images, int octvs){
 	}
 	return 0;
 }
-
 
 int PyramidKDoG(vector<Mat> & PyKDoG, int octvs, int intvls){
 	vector<double> sig;
@@ -147,56 +136,63 @@ int PyramidDoG(Mat Image, vector<Mat> PyDoG){
 	vector<Mat> images;
 	PyramidKDoG( PyKDoG,octvs,intvls);
 	ResizeImage(Image,images,octvs);
-	ArrayImage * imgs= new ArrayImage[images.size()];
-	ArrayImage * pkDoG= new ArrayImage[PyKDoG.size()];
-	VectorToPointer(images,imgs);
-	VectorToPointer(PyKDoG,pkDoG);
+
+	
+		
+	ArrayImage * pyDoG_Out;
+	
 	////////////////////////////////////////////////////////////////////////////////////////
 	/////////////////////////////////////////////////////////////////////Reservo Memoria GPU
-	ArrayImage * imgs_D;
-	ArrayImage * pkDoG_D;
-	ArrayImage * pyDoG_Out;
-	e=cudaMalloc(&imgs_D,sizeof(ArrayImage)*images.size());///imagenes
-	cout<<cudaGetErrorString(e)<<" cudaMalloc"<<endl;
-	
-
-	e=cudaMalloc(&pkDoG_D,sizeof(ArrayImage)*PyKDoG.size());//mascaras
-	cout<<cudaGetErrorString(e)<<" cudaMalloc"<<endl;
-	
-
 	e=cudaMalloc(&pyDoG_Out,sizeof(ArrayImage)*images.size()*sizeof(ArrayImage) *PyKDoG.size());
 	cout<<cudaGetErrorString(e)<<" cudaMalloc"<<endl;
 
-	////////////////////////////////////////////////////////////////////////////////////////
-	/////////////////////////////////////////////////////////////////////Copio Memoria GPU
-	e=cudaMemcpy(imgs_D,imgs,sizeof(ArrayImage)*images.size(),cudaMemcpyHostToDevice);
-	cout<<cudaGetErrorString(e)<<" cudaMemCopyHD"<<endl;
-	e=cudaMemcpy(pkDoG_D,pkDoG,sizeof(ArrayImage)*PyKDoG.size(),cudaMemcpyHostToDevice);
-	cout<<cudaGetErrorString(e)<<" cudaMemCopyHD"<<endl;
-	////////////////////////////////////////////////////////////////////////////////////////
-	/////////////////////////////////////////////////////////////////////Lanzo Kernel
-	for (int i = 0; i < images.size(); ++i)
+
+	for (int i = 0; i < images.size() ; ++i)
 	{
-		ConvolutionDoG<<<16,32>>>(imgs_D,pkDoG_D,pyDoG_Out,i,PyKDoG.size());/////i es el indice de la imagen que se va a emborrona
-		cudaDeviceSynchronize();
-		cout<<""<<endl;
-		cout<<i<<endl;
-		cout<<""<<endl;
+		
+		float * img_D;
+		int sizeImage=images[i].rows*images[i].cols;
+		////////////////////////////////////////////////////////////////////////////////////////
+		/////////////////////////////////////////////////////////////////////Reservo Memoria GPU
+		e=cudaMalloc(&img_D,sizeof(float)*sizeImage);///imagenes
+		cout<<cudaGetErrorString(e)<<" cudaMalloc"<<endl;
+
+		////////////////////////////////////////////////////////////////////////////////////////
+		/////////////////////////////////////////////////////////////////////Copio Memoria GPU
+		e=cudaMemcpy(img_D,images[i].ptr<float>(),sizeof(float)*sizeImage,cudaMemcpyHostToDevice);
+		cout<<cudaGetErrorString(e)<<" cudaMemCopyHD"<<endl;
+
+		
+		for (int m = 0; m < PyKDoG.size(); ++i)
+		{
+			float * pkDoG_D;
+			int sizeMask=PyKDoG[j].rows*PyKDoG[j].cols;
+
+			////////////////////////////////////////////////////////////////////////////////////////
+			/////////////////////////////////////////////////////////////////////Reservo Memoria GPU
+			e=cudaMalloc(&pkDoG_D,sizeof(float)*sizeMask);//mascaras
+			cout<<cudaGetErrorString(e)<<" cudaMalloc"<<endl;
+
+			////////////////////////////////////////////////////////////////////////////////////////
+			/////////////////////////////////////////////////////////////////////Copio Memoria GPU
+			e=cudaMemcpy(pkDoG_D,PyKDoG[j].ptr<float>(),sizeof(float)*sizeMask,cudaMemcpyHostToDevice);
+			cout<<cudaGetErrorString(e)<<" cudaMemCopyHD"<<endl;
+
+			////////////////////////////////////////////////////////////////////////////////////////
+			/////////////////////////////////////////////////////////////////////Lanzo Kernel
+			Blur<<<16,32>>>();
+			cudaDeviceSynchronize();
+			cudaFree(pkDoG_D); 
+		}
+		
+		e=cudaMemcpy(,,,cudaMemcpyDeviceToHost);
+		cout<<cudaGetErrorString(e)<<" cudaMemCopyDH"<<endl;
+		cudaFree(imgs_D);
+		
+
 	}
-	//cout<< PyKDoG.size() <<endl;
-	
-	ArrayImage * img_out_test = new ArrayImage[sizeof(ArrayImage)*images.size()*sizeof(ArrayImage)*PyKDoG.size()];
-
-	e=cudaMemcpy(img_out_test,pyDoG_Out,sizeof(ArrayImage)*images.size()*sizeof(ArrayImage)*PyKDoG.size(),cudaMemcpyDeviceToHost);
-	cout<<cudaGetErrorString(e)<<" cudaMemCopyDH"<<endl;
-
-
-
-
-	cudaFree(imgs_D);
-	cudaFree(pkDoG_D);
 	cudaFree(pyDoG_Out);
-	free(img_out_test);
+
 
 	return 0;
 }
